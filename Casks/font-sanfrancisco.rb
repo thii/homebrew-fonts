6ffcbfaebba5d5c3f22d913188b8e9605d1fc240
Doan Truong Thi <t@thi.im>
2014-11-19 23:39:18 +0900

    Add San Francisco Text – the Watch font

---
+++ Casks/font-sanfrancisco.rb
@@ -0,0 +1,15 @@
+cask :v1 => 'font-sanfrancisco' do
+  version 'latest'
+  sha256 '20d051562b9cc354c2a36947aaab4f579cf48045df527fbbe26268bdfd9e77a1'
+
+  url 'https://github.com/wellsriley/YosemiteSanFranciscoFont/archive/master.zip'
+  homepage 'https://developer.apple.com/watchkit/'
+  license :unknown
+
+  font 'System San Francisco Text Bold.ttf'
+  font 'System San Francisco Text Heavy.ttf'
+  font 'System San Francisco Text Medium.ttf'
+  font 'System San Francisco Text Regular.ttf'
+  font 'System San Francisco Text Thin.ttf'
+  font 'System San Francisco Display Ultralight.ttf'
+end
